6ec4b27d91388252d44dd48c5b43c7526956c7e5
Justin Keeling <jrkeeling23@gmail.com>
2025-06-01 14:28:57 -0600

    added pictures to services, updated text in services, and updated text in about us

--- src/pages/aboutUs/index.tsx
+++ src/pages/aboutUs/index.tsx
@@ -1,25 +1,37 @@
 import { Box, Typography } from "@mui/material";
 
 export const AboutUs = () => {
-    return (
-        <Box
-            display="flex"
-            flexDirection={"column"}
-            alignItems={"center"}
-            gap='5rem'
-        >
-            <Box display="flex" flexDirection={"column"} gap="1rem" >
-                <Typography variant="h4">About Us</Typography>
-                <Typography>
-                    We offer expert tree trimming, removal, pruning, and storm cleanup
-                    services for residential and commercial properties. No matter the
-                    size of the job, we're equipped to keep your trees — and your
-                    property — safe, healthy, and looking their best.
-                </Typography>
-            </Box>
-
-
-        </Box>
-
-    );
+  return (
+    <Box
+      display="flex"
+      flexDirection={"column"}
+      alignItems={"center"}
+      gap="5rem"
+    >
+      <Box display="flex" flexDirection={"column"} gap="1rem">
+        <Typography variant="h4">About Us</Typography>
+        <Typography>
+          At All Tree Works, we believe every tree tells a story—and we're here
+          to help yours thrive. Founded in 2025 in the heart of Billings,
+          Montana, our mission is to provide reliable, honest, and expert tree
+          care services to homeowners, ranchers, and businesses across the
+          region.
+          <br />
+          <br />
+          As a locally owned and operated company, we understand the unique
+          needs of Montana's trees and terrain. From pruning and removals to
+          emergency storm cleanup and stump grinding, our certified arborists
+          and skilled crew bring their experience, a deep respect for nature,
+          and a commitment to safety and customer satisfaction to every job.
+          <br />
+          <br />
+          We're more than just a tree company—we're your neighbors, and we treat
+          your property like our own. Whether you're preserving a legacy tree,
+          clearing land for new growth, or simply keeping your yard safe and
+          beautiful, All Tree Works is here to help. Let us take care of the
+          hard work, so you can enjoy the beauty of the trees around you.
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -1,25 +1,120 @@
 import { Box, Typography } from "@mui/material";
+import vehicle from "../../assets/images/vehicle.jpeg";
+import afterTree1_0 from "../../assets/images/after-tree-1.0.jpeg";
+import afterTree1_1 from "../../assets/images/after-tree-1.1.jpeg";
+import beforeTree2_0 from "../../assets/images/before-tree-2.0.jpeg";
+import afterTree2_0 from "../../assets/images/after-tree-2.0.jpeg";
 
 export const Services = () => {
-    return (
-        <Box
+  return (
+    <Box
+      display="flex"
+      flexDirection={"column"}
+      alignItems={"center"}
+      gap="5rem"
+    >
+      <Box display="flex" flexDirection={"column"} gap="1rem">
+        <Typography variant="h4">What We Do</Typography>
+        <Typography>
+          At All Tree Works, we specialize in two essential services to keep
+          your property safe, beautiful, and thriving: tree removal and tree
+          pruning.
+        </Typography>
+        <Typography variant="h5">Tree Removal</Typography>
+        <Box display={"flex"} flexDirection={"column"} gap="2rem">
+          <Typography>
+            Sometimes, a tree has to come down—whether it's dead, diseased,
+            leaning dangerously, or in the way of new construction. Our team is
+            equipped with the skills and tools to safely and efficiently remove
+            trees of all sizes. Using a combination of lift trucks and advanced
+            climbing gear, we tailor our approach to suit the unique challenges
+            of each removal. We always work with safety, efficiency, and
+            property protection as our top priorities.
+          </Typography>
+          <Box
             display="flex"
-            flexDirection={"column"}
-            alignItems={"center"}
-            gap='5rem'
-        >
-            <Box display="flex" flexDirection={"column"} gap="1rem" >
-                <Typography variant="h4">What We Do</Typography>
-                <Typography>
-                    We offer expert tree trimming, removal, pruning, and storm cleanup
-                    services for residential and commercial properties. No matter the
-                    size of the job, we’re equipped to keep your trees — and your
-                    property — safe, healthy, and looking their best.
-                </Typography>
-            </Box>
-
-           
+            gap="2rem"
+            flexWrap={"wrap"}
+            justifyContent={"space-evenly"}
+          >
+            <Box
+              component="img"
+              src={beforeTree2_0}
+              alt="All Tree Works Vehicle"
+              sx={{
+                width: "25rem",
+                //   height: "auto",
+                borderRadius: "8px",
+              }}
+            />
+            <Box
+              component="img"
+              src={afterTree2_0}
+              alt="All Tree Works Vehicle"
+              sx={{
+                width: "25rem",
+                //   height: "auto",
+                borderRadius: "8px",
+              }}
+            />
+          </Box>
         </Box>
 
-    );
+        {/* <Image href={''}></Image> */}
+        <Typography variant="h5">Tree Pruning</Typography>
+        <Box display="flex" flexDirection="column" gap="2rem">
+          <Typography>
+            Proper pruning promotes healthy growth, improves tree structure, and
+            enhances the appearance of your landscape. Whether you're looking to
+            shape young trees, remove dead or damaged limbs, or reduce weight on
+            heavy branches, our expert climbers and bucket truck operators will
+            get the job done with precision and care. We understand the biology
+            of trees and prune with long-term health in mind.
+            <br />
+            <br />
+            No matter the scope of the job, you can count on All Tree Works to
+            deliver professional service with a neighborly touch. We're here to
+            keep Billings' trees strong, safe, and stunning
+          </Typography>
+          <Box
+            display="flex"
+            gap="2rem"
+            flexWrap={"wrap"}
+            justifyContent={"space-evenly"}
+          >
+            <Box
+              component="img"
+              src={vehicle}
+              alt="All Tree Works Vehicle"
+              sx={{
+                width: "25rem",
+                //   height: "auto",
+                borderRadius: "8px",
+              }}
+            />
+            <Box
+              component="img"
+              src={afterTree1_0}
+              alt="All Tree Works Vehicle"
+              sx={{
+                width: "25rem",
+                //   height: "auto",
+                borderRadius: "8px",
+              }}
+            />
+            <Box
+              component="img"
+              src={afterTree1_1}
+              alt="All Tree Works Vehicle"
+              sx={{
+                width: "25rem",
+                //   height: "auto",
+                borderRadius: "8px",
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
