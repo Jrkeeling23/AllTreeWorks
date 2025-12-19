9a9bc765a49559f25fdcfa97083fd8df32b70dbf
Justin Keeling <jrkeeling23@gmail.com>
2025-12-18 18:27:26 -0700

    fixing?

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import svgr from "@svgr/rollup";
 
 export default defineConfig({
-    base: '/AllTreeWorks/',
+    base: '/',
     plugins: [react(), svgr()],
     server: {
         host: '0.0.0.0',  // or 'localhost' or a custom IP
